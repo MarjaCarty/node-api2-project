508bfed0b56c5d8c083e51f0cf652a9a8530b390
MarjaCarty <marja.d.carty@gmail.com>
2020-12-01 16:46:59 -0600

    working

--- api/postsRouter.js
+++ api/postsRouter.js
@@ -29,9 +29,62 @@ router.post("/", async (req, res) => {
     });
   }
 });
-router.get("/:id", (req, res) => {});
-router.put("/:id", (req, res) => {});
-router.delete("/:id", (req, res) => {});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved." });
+  }
+});
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!req.body.title || !req.body.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  }
+
+  try {
+    const updatedPost = await Post.update(id, req.body);
+    if (updatedPost) {
+      res.status(200).json(updatedPost);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ error: "The post information could not be modified." });
+  }
+});
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedPost = await Post.delete(id);
+    if (deletedPost) {
+      res.status(200).json({ message: "The post was removed" });
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch {
+    res.status(500).json({ error: "The post could not be removed" });
+  }
+});
 router.get("/:id/comments", (req, res) => {});
 router.post("/:id/comments", (req, res) => {});
 
